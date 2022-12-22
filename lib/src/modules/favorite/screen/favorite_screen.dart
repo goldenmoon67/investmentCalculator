9368771d930775b38ef2cb30a9d71a7b5407ab2c
goldenmoon67 <abdulmirac@gmail.com>
2022-12-23 02:38:28 +0300

    some kind of design issues

--- lib/src/modules/favorite/screen/favorite_screen.dart
+++ lib/src/modules/favorite/screen/favorite_screen.dart
@@ -1,8 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:crypto_price/src/consts/colors/app_colors.dart';
 import 'package:crypto_price/src/data/models/crypto/crypto.dart';
 import 'package:crypto_price/src/data/models/favorite/favorite_model.dart';
 import 'package:crypto_price/src/modules/favorite/bloc/favorite_event.dart';
-import 'package:crypto_price/src/widgets/components/result_screen_components/investment_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
@@ -43,14 +43,11 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
           Widget content = Container();
 
           if (state is FavoriteStartData) {
-            debugPrint("budayiz");
-
             content = _getContent(state);
           }
           return Scaffold(
             backgroundColor: Colors.white,
             body: SingleChildScrollView(
-              physics: const NeverScrollableScrollPhysics(),
               child: content,
             ),
             resizeToAvoidBottomInset: false,
@@ -61,33 +58,132 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
   }
 
   Widget _getContent(FavoriteStartData state) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 2 / 3,
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          var item = state.items[index];
-          return _getWidget(
-              context,
-              index,
-              item ??
-                  FavoriteModel(
-                      crypto: Crypto(icon: "", name: "", symbol: ""),
+    return Column(
+      children: [
+        Stack(
+          children: [
+            ClipRRect(
+              borderRadius:
+                  const BorderRadius.vertical(bottom: Radius.circular(70)),
+              child: Image.asset(
+                "assets/images/fav_banner.jpg",
+                fit: BoxFit.fill,
+              ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                margin: const EdgeInsets.only(
+                  top: 40,
+                ),
+                child: const Text(
+                  "Recorded",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 2 / 3,
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemBuilder: (context, index) {
+              var item = state.items[index];
+              return _getWidget(
+                context,
+                index,
+                item ??
+                    FavoriteModel(
+                      crypto: Crypto(
+                        icon: "",
+                        name: "",
+                        symbol: "",
+                      ),
                       priceResult: null,
-                      percentResult: null));
-        },
-        itemCount: 2,
-      ),
+                      percentResult: null,
+                    ),
+              );
+            },
+            itemCount: 4,
+          ),
+        ),
+      ],
     );
   }
 
   _getWidget(BuildContext context, int index, FavoriteModel item) {
-    return InvestmentItem(
-      cryptoSymbol: item.crypto.symbol,
-      cryptoName: item.crypto.name,
-      howMuch: 3,
-      currentRange: 3,
-      investmentItemType: InvestmentItemType.price,
-      imageUrl: item.crypto.icon ?? "",
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+      child: Container(
+        height: 130,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: AppColors.greyColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 50,
+                    child: AspectRatio(
+                      aspectRatio: 1,
+                      child: ClipRRect(
+                        child: CachedNetworkImage(
+                          imageUrl: item.crypto.icon ?? "",
+                          fit: BoxFit.cover,
+                          errorWidget: (context, url, error) {
+                            return const Center(
+                              child: Icon(
+                                Icons.dangerous,
+                                color: Colors.black,
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    item.crypto.symbol,
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Column(
+                children: [
+                  Text(
+                    item.crypto.name,
+                  ),
+                  Row(
+                    children: [
+                      const Text("Expecting:"),
+                      Text(
+                        item.percentResult != null
+                            ? item.percentResult!.percentWeWant.toString()
+                            : item.priceResult!.expectingProfit.toString(),
+                      ),
+                      Text(
+                        item.percentResult != null ? " \$" : " %",
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
